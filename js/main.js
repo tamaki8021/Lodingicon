d913348b2eb52be877d4b81654fbf7d743d24c62
tamaki <hbk2002320@gmail.com>
2020-10-23 00:58:16 +0900

    add orangepoint moving to main.js

--- js/main.js
+++ js/main.js
@@ -2,20 +2,27 @@
 
 (() => {
   class Icon {
-    constructor(canvas) {
+    constructor(canvas) {  //描写コンテクストが必要
       this.ctx = canvas.getContext('2d');
       this.width = canvas.width;
       this.height = canvas.height;
-      this.r = 60;
+      this.r = 60;  //半径
+
+      this.angle = 0;  //度数の初期設定
     }
 
     draw() {
-      this.ctx.translate(this.width / 2, this.height / 2);
+      this.ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';  //30%くらいの白にする
+      this.ctx.fillRect(0, 0, this.width, this.height);  //塗りつぶす
 
-      this.ctx.beginPath();
-      // this.ctx.arc(this.width / 2, this.height / 2, this.r, 0, 2 * Math.PI);
-      this.ctx.arc(0, 0, this.r, 0, 2 * Math.PI);
-      this.ctx.stroke();
+      this.ctx.save(); //セーブする
+      this.ctx.translate(this.width / 2, this.height / 2);
+      this.ctx.rotate(Math.PI / 180 * this.angle);
+
+      // this.ctx.beginPath();
+      // // this.ctx.arc(this.width / 2, this.height / 2, this.r, 0, 2 * Math.PI);   //半径はr, そして0度から360度まで
+      // this.ctx.arc(0, 0, this.r, 0, 2 * Math.PI);
+      // this.ctx.stroke();   //丸円のコード
 
       this.ctx.beginPath();
       // this.ctx.moveTo(this.width / 2, this.height / 2, this.r - 5);
@@ -25,10 +32,21 @@
       this.ctx.strokeStyle = 'orange';
       this.ctx.lineWidth = 6;
       this.ctx.stroke();
+
+      this.ctx.restore();   //原点がもとに戻る
+    }
+
+    update() {
+      this.angle += 12;  //12度づつ動く
     }
 
     run() {
+      this.update();
       this.draw();
+
+      setTimeout(() => {
+        this.run();
+      }, 100);  //run()メソッドを100ミリ秒後に繰り返し行なう
     }
   }
   const canvas = document.querySelector('canvas');
